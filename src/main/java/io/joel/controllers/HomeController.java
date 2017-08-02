ab69e66330fd2bd7003042fcb3207c646feb7abf
taddeimania <jtaddei@gmail.com>
2017-08-02 11:17:07 -0400

    Initial

---
+++ src/main/java/io/joel/controllers/HomeController.java
@@ -0,0 +1,4 @@
+package io.joel.controllers;
+
+public class HomeController {
+}
---
+++ src/test/java/io/joel/ThymeleaffunApplicationTests.java
@@ -0,0 +1,16 @@
+package io.joel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThymeleaffunApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
